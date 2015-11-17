fe07c2962a14699f5ca5e58107474b3a4468e929
Tobias Giess <tobias.giess@gmail.com>
2015-11-17 17:21:48 +0100

    Added new function that return the optimal number of hash functions

--- src/main/java/ch/fhnw/main/Main.java
+++ src/main/java/ch/fhnw/main/Main.java
@@ -6,5 +6,18 @@ public class Main {
         System.out.println("Start App");
 
     }
+    
+    /**
+     * Berechnet die optimale Anzahl k an Hashfunktionen.
+     * 
+     * @param inFilterSize eine geeignete Filtergrösse m.
+     * @param inNumberOfInsertedElements Anzahl n an zu erwartenden Elementen.
+     * @return optimale Anzahl k an Hashfunktionen
+     */
+    private int getOptimalNumberOfHashfunctions(int inFilterSize, int inNumberOfInsertedElements){
+    	int m = inFilterSize;
+    	int n = inNumberOfInsertedElements;
+    	return (int) ( m / n * Math.log(2) );
+    }
 
 }
